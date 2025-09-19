1e4c5e2bb4ee79ca8fd526ac3860a2a5b05babc8
sergii.ignatov <s.ignatov92@gmail.com>
2025-09-19 16:02:26 +0300

    App setup and skeleton

---
+++ app/routes/home/index.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/index";
+
+export function meta({}: Route.MetaArgs) {
+    return [
+        { title: "RC Dev site | Welcome" },
+        { name: "description", content: "Custom portfolio site development" },
+    ];
+}
+
+export default function Home() {
+    return <section>My App</section>;
+}
