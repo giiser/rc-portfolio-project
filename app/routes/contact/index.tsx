d394e7535a4adea434592b1191688cbf285b778e
sergii.ignatov <s.ignatov92@gmail.com>
2025-09-24 18:54:37 +0300

    Added Form Action

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,10 +1,32 @@
-const ContactPage = () => {
+import {Form} from 'react-router';
+import type { Route } from "./+types";
+
+export async function action({request}:Route.ActionArgs) {
+    const formData = await request.formData();
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const subject = formData.get('subject');
+    const message = formData.get('message');
+
+    const data = {name, email, subject, message};
+
+    return {message:'Form submitted successfully', data};
+}
+
+const ContactPage = ({actionData}:Route.ComponentProps) => {
     return (
         <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
             <h2 className="text-3xl font-bold text-white mb-8 text-center">
                 📪 Contact Me
             </h2>
-            <form className="space-y-6">
+
+            {actionData?.message ? (
+                <p className="mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md">
+                    {actionData.message}
+                </p>
+            ) : null}
+
+            <Form method="post" className="space-y-6">
                 <div>
                     <label htmlFor="name" className="block text-sm font-medium text0gray-300">Full Name</label>
                     <input type="text" id="name" name="name" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"/>
@@ -22,7 +44,7 @@ const ContactPage = () => {
                     <textarea id="message" name="message" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"/>
                 </div>
                 <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 cursor-pointer">Send Message</button>
-            </form>
+            </Form>
         </div>
     )
 }
