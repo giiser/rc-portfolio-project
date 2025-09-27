11609db19f4afe3f5b60e020eef6b997a06d771f
sergii.ignatov <s.ignatov92@gmail.com>
2025-09-27 21:25:47 +0300

    connected form to formspree.io

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,35 +1,7 @@
-import {Form} from 'react-router';
 import type { Route } from "./+types";
 
-export async function action({request}:Route.ActionArgs) {
-    const formData = await request.formData();
-    const name = formData.get('name') as string;
-    const email = formData.get('email') as string;
-    const subject = formData.get('subject') as string;
-    const message = formData.get('message') as string;
-
-    const errors:Record<string, string> = {};
-    if (!name) errors.name = 'Name is required';
-    if (!email) {
-        errors.email = 'Email is required';
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
-        errors.email = 'Email is invalid';
-    }
-    if (!subject) errors.subject = 'Subject is required';
-    if (!message) errors.message = 'Message is required';
-
-    if(Object.keys(errors).length > 0){
-        return {errors};
-    }
-
-    const data = {name, email, subject, message};
-
-    return {message:'Form submitted successfully', data};
-}
 
 const ContactPage = ({actionData}:Route.ComponentProps) => {
-
-    const errors = actionData?.errors || {};
 
     return (
         <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
@@ -37,35 +9,26 @@ const ContactPage = ({actionData}:Route.ComponentProps) => {
                 📪 Contact Me
             </h2>
 
-            {actionData?.message ? (
-                <p className="mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md">
-                    {actionData.message}
-                </p>
-            ) : null}
 
-            <Form method="post" className="space-y-6">
+            <form method="post" className="space-y-6" action="https://formspree.io/f/mldpgwkd">
                 <div>
                     <label htmlFor="name" className="block text-sm font-medium text0gray-300">Full Name</label>
                     <input type="text" id="name" name="name" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"/>
-                    {errors.name && <p className="mt-1 text-sm text-red-400">{errors.name}</p>}
                 </div>
                 <div>
                     <label htmlFor="email" className="block text-sm font-medium text0gray-300">E-Mail</label>
                     <input type="email" id="email" name="email" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"/>
-                    {errors.email && <p className="mt-1 text-sm text-red-400">{errors.email}</p>}
                 </div>
                 <div>
                     <label htmlFor="subject" className="block text-sm font-medium text0gray-300">Subject</label>
                     <input type="text" id="subject" name="subject" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"/>
-                    {errors.subject && <p className="mt-1 text-sm text-red-400">{errors.subject}</p>}
                 </div>
                 <div>
                     <label htmlFor="message" className="block text-sm font-medium text0gray-300">Message</label>
                     <textarea id="message" name="message" className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"/>
-                    {errors.message && <p className="mt-1 text-sm text-red-400">{errors.message}</p>}
                 </div>
                 <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 cursor-pointer">Send Message</button>
-            </Form>
+            </form>
         </div>
     )
 }
